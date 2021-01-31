55b5b2e6e4a71b38e881c427b043d98994bda57f
kawabata_shintaro <4499135+kawabatas@users.noreply.github.com>
2021-01-31 14:59:23 +0900

    Implement mysql exec cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use dotenv::dotenv;
+use mysql::prelude::*;
+use mysql::*;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = Pool::new(database_url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    let mut file = File::open("sql/exec.sql").expect("file not found");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("something went wrong reading the file");
+    let queries: Vec<&str> = data.trim().split("\n").collect();
+
+    for q in queries {
+        println!("{}", q);
+        conn.query_iter(q).unwrap();
+    }
 }
